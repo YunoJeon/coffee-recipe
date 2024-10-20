df934b641c6613ced1cb0ecc23d7bd234a789225
jeon-yuno <zero0577@naver.com>
2024-10-20 19:34:13 +0900

    base : [000 - Project Base Setup]

---
+++ src/main/java/com/coffee/coffeeserviceproject/CoffeeServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.coffeeserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeServiceProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoffeeServiceProjectApplication.class, args);
+  }
+
+}
